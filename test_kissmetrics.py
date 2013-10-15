da5a8f5d9595debd84eb037db0b13debc8c50445
Ernest W. Durbin III <edurbin@kissmetrics.com>
2013-10-15 10:07:39 -0400

    test timestamp passing

--- test_kissmetrics.py
+++ test_kissmetrics.py
@@ -46,18 +46,30 @@ class KISSmetricsRequestTestCase(unittest.TestCase):
     request = KISSmetrics.Request(key='foo', person='bar', identity='baz')
     assert request.query_string == '_n=baz&_k=foo&_p=bar'
 
+  def test_timestamp(self):
+    request = KISSmetrics.Request(key='foo', person='bar', timestamp=1381849312)
+    assert request.query_string == '_t=1381849312&_d=1&_k=foo&_p=bar'
+
 class KISSmetricsRequestFunctionsTestCase(unittest.TestCase):
 
   def test_record(self):
     query_string = KISSmetrics.request.record(key='foo', person='bar', event='fizzed')
     assert query_string == "_n=fizzed&_k=foo&_p=bar"
 
+  def test_record_with_timestamp(self):
+    query_string = KISSmetrics.request.record(key='foo', person='bar', event='fizzed', timestamp=1381849312)
+    assert query_string == "_t=1381849312&_d=1&_k=foo&_n=fizzed&_p=bar"
+
   def test_set(self):
     properties = {'cool': '1'}
     query_string = KISSmetrics.request.set(key='foo', person='bar', properties=properties)
     assert query_string == 'cool=1&_k=foo&_p=bar'
 
+  def test_set_with_timestamp(self):
+    properties = {'cool': '1'}
+    query_string = KISSmetrics.request.set(key='foo', person='bar', properties=properties, timestamp=1381849312)
+    assert query_string == '_t=1381849312&cool=1&_d=1&_k=foo&_p=bar'
+
   def test_alias(self):
     query_string = KISSmetrics.request.alias(key='foo', person='bar', identity='baz')
     assert query_string == '_n=baz&_k=foo&_p=bar'
-
