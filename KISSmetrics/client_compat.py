de046062ae92ec3ded710236156192dfce00acdd
Ernest W. Durbin III <edurbin@kissmetrics.com>
2013-10-15 21:31:11 -0400

    pep8

--- KISSmetrics/client_compat.py
+++ KISSmetrics/client_compat.py
@@ -7,7 +7,8 @@ from KISSmetrics import Client
 
 class ClientCompat:
 
-    def __init__(self, key, trk_host=KISSmetrics.TRACKING_HOSTNAME, trk_proto=KISSmetrics.TRACKING_PROTOCOL):
+    def __init__(self, key, trk_host=KISSmetrics.TRACKING_HOSTNAME,
+                 trk_proto=KISSmetrics.TRACKING_PROTOCOL):
         self.client = Client(key=key, trk_host=trk_host, trk_proto=trk_proto)
         self.identity = None
 
@@ -17,11 +18,16 @@ class ClientCompat:
     def url(self, query_string):
         return self.client.url(query_string)
 
-    def record(self, event, properties={}, timestamp=None, uri=KISSmetrics.RECORD_URI):
-        return self.client.record(person=self.identity, event=event, properties=properties, timestamp=timestamp, uri=uri)
+    def record(self, event, properties={}, timestamp=None,
+               uri=KISSmetrics.RECORD_URI):
+        return self.client.record(person=self.identity, event=event,
+                                  properties=properties, timestamp=timestamp,
+                                  uri=uri)
 
     def set(self, properties={}, timestamp=None, uri=KISSmetrics.SET_URI):
-        return self.client.set(person=self.identity, properties=properties, timestamp=timestamp, uri=uri)
+        return self.client.set(person=self.identity, properties=properties,
+                               timestamp=timestamp, uri=uri)
 
     def alias(self, identity, uri=KISSmetrics.ALIAS_URI):
-        return self.client.alias(person=self.identity, identity=identity, uri=uri)
+        return self.client.alias(person=self.identity, identity=identity,
+                                 uri=uri)
