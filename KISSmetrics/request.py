f478cb1f17ba183db50988db6c7966d92fff8005
Ernest W. Durbin III <ewdurbin@gmail.com>
2013-11-13 14:52:20 -0500

    reimplement _request method to wrap urlparse

--- KISSmetrics/request.py
+++ KISSmetrics/request.py
@@ -9,15 +9,20 @@ except ImportError:
     from urllib.parse import urlunsplit, SplitResult
 
 
+def _request(scheme=None, netloc=None, path=None, query=None, fragment=None):
+    split = SplitResult(scheme=scheme, netloc=netloc, path=path, query=query,
+                        fragment=None)
+    return urlunsplit(split)
+
+
 def record(key, person, event, timestamp=None, properties=None,
            scheme=KISSmetrics.TRACKING_SCHEME,
            host=KISSmetrics.TRACKING_HOSTNAME,
            path=KISSmetrics.RECORD_PATH):
     query = create_query(key, person, event=event, timestamp=timestamp,
                          properties=properties)
-    split = SplitResult(scheme=scheme, netloc=host, path=path, query=query,
-                        fragment=None)
-    return urlunsplit(split)
+    return _request(scheme=scheme, netloc=host, path=path, query=query,
+                    fragment=None)
 
 
 def set(key, person, timestamp=None, properties=None,
@@ -26,9 +31,8 @@ def set(key, person, timestamp=None, properties=None,
         path=KISSmetrics.SET_PATH):
     query = create_query(key, person, timestamp=timestamp,
                          properties=properties)
-    split = SplitResult(scheme=scheme, netloc=host, path=path, query=query,
-                        fragment=None)
-    return urlunsplit(split)
+    return _request(scheme=scheme, netloc=host, path=path, query=query,
+                    fragment=None)
 
 
 def alias(key, person, identity,
@@ -36,6 +40,5 @@ def alias(key, person, identity,
           host=KISSmetrics.TRACKING_HOSTNAME,
           path=KISSmetrics.ALIAS_PATH):
     query = create_query(key, person, identity=identity)
-    split = SplitResult(scheme=scheme, netloc=host, path=path, query=query,
-                        fragment=None)
-    return urlunsplit(split)
+    return _request(scheme=scheme, netloc=host, path=path, query=query,
+                    fragment=None)
