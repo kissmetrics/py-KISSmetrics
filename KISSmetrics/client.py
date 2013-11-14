29bd015a9b831704ac51c2415037138fd45f1525
Nick Murphy <njmurphy@gmail.com>
2013-11-14 11:07:31 -0600

    Add omitted changes from master

--- KISSmetrics/client.py
+++ KISSmetrics/client.py
@@ -6,11 +6,23 @@ from urllib3 import PoolManager
 
 
 class Client(object):
+    """Interface to KISSmetrics tracking service"""
 
     def __init__(self, key, trk_host=KISSmetrics.TRACKING_HOSTNAME,
                  trk_scheme=KISSmetrics.TRACKING_SCHEME):
+        """Initialize client for use with KISSmetrics API key.
+
+        :param key: API key for product, found on the
+                    "KISSmetrics Settings".
+        :type key: str
+        :param trk_host: tracking host for requests; defaults
+                         production tracking service.
+        :param trk_proto: the protocol for requests; either be `'http'`
+                          or `'https'`.
+
+        """
         self.key = key
-        if trk_scheme not in ['http', 'https']:
+        if trk_scheme not in ('http', 'https'):
             raise ValueError('trk_scheme must be one of (http, https)')
         self.http = PoolManager()
         self.trk_host = trk_host
@@ -21,6 +33,21 @@ class Client(object):
 
     def record(self, person, event, properties=None, timestamp=None,
                path=KISSmetrics.RECORD_PATH):
+        """Record `event` for `person` with any `properties`.
+
+        :param person: the individual performing the `event`
+        :param event: the `event` name that was performed
+        :param properties: any additional data to include
+        :type properties: dict
+        :param timestamp: when the `event` was performed; optional for
+                          back-dating
+        :param path: HTTP endpoint to use; defaults to
+                    ``KISSmetrics.RECORD_PATH``
+
+        :returns: an HTTP response for the request
+        :rtype: `urllib3.response.HTTPResponse`
+
+        """
         this_request = request.record(self.key, person, event,
                                       timestamp=timestamp,
                                       properties=properties,
@@ -30,6 +57,20 @@ class Client(object):
 
     def set(self, person, properties=None, timestamp=None,
             path=KISSmetrics.SET_PATH):
+        """Set a property (or properties) for a `person`.
+
+        :param person: individual to associate properties with
+        :param properties: key-value pairs to associate with `person`
+        :type properties: dict
+        :param timestamp: when the `event` was performed; optional for
+                          back-dating
+        :param path: HTTP endpoint to use; defaults to
+                    ``KISSmetrics.SET_PATH``
+
+        :returns: an HTTP response for the request
+        :rtype: `urllib3.response.HTTPResponse`
+
+        """
         this_request = request.set(self.key, person, timestamp=timestamp,
                                    properties=properties,
                                    scheme=self.trk_scheme, host=self.trk_host,
@@ -37,6 +78,37 @@ class Client(object):
         return self.request(this_request)
 
     def alias(self, person, identity, path=KISSmetrics.ALIAS_PATH):
+        """Map `person` to `identity`; actions done by one resolve to other.
+
+        :param person: consider as same individual ``identity``; the
+                       source of the alias operation
+        :type person: str or unicode
+        :param identity: consider as an alias of ``person``; the target
+                         of the alias operation
+        :type identity: str or unicode
+        :param path: HTTP endpoint to use; defaults to
+                    ``KISSmetrics.ALIAS_PATH``
+
+        :returns: an HTTP response for the request
+        :rtype: `urllib3.response.HTTPResponse`
+
+        Note the direction of the mapping is ``person`` to ``identity``
+        (so "``person`` is also known as ``identity``" or "``person`` =>
+        ``identity``" when looking at it as "``<source>`` => ``<target>``")
+
+        When consulting the Aliasing documentation, `person` corresponds
+        to ``query_string.PERSON_PARAM`` and `identity` corresponds to
+        ``query_string.ALIAS_PARAM``.
+
+        Aliasing is not a reversible operation.  When aliasing to an
+        identity, take care not to use a session identifier or any other
+        value that is not relatively stable (a value that will not
+        change per request or per session).
+
+        For more information see the API Specifications on `Aliasing
+        <http://support.kissmetrics.com/apis/specifications.html#aliasing-users>`_.
+
+        """
         this_request = request.alias(self.key, person, identity,
                                      scheme=self.trk_scheme,
                                      host=self.trk_host, path=path)
--- KISSmetrics/client_compat.py
+++ KISSmetrics/client_compat.py
@@ -10,8 +10,28 @@ port_map = {'80': 'http', '443': 'https'}
 
 
 class ClientCompat(object):
+    """Compatibility interface to KISSmetrics tracking service
+
+    .. warning::
+
+        Interface only exists for compatibility and will not be supported
+        in the future.
+
+    """
 
     def __init__(self, key, host=None, http_timeout=None, logging=True):
+        """Initialize client for use with KISSmetrics API key.
+
+        :param key: API key for a product, found on the
+                    "KISSmetrics Settings".
+        :type key: str
+        :param host: tracking host for requests; defaults
+                     production tracking service.
+        :param http_timeout: request timeout; defaults to None
+        :type http_timeout: int
+        :param logging: indicate whether to log
+
+        """
         self.key = key
         if host:
             (trk_host, trk_port) = host.split(':')
@@ -26,10 +46,30 @@ class ClientCompat(object):
         self.identity = None
 
     def identify(self, identity):
+        """Define identity for subsequent calls.
+
+        :param identity: identifying info (email, user-id, anonymous-id)
+        :type identity: str or unicode
+
+        """
         self.identity = identity
 
     def record(self, action, props=None, path=KISSmetrics.RECORD_PATH,
                resp=False):
+        """Record event for identity with any properties.
+
+        :param action: event performed
+        :param props: any additional data to include
+        :type props: dict
+        :param resp: indicate whether to return response
+        :type resp: boolean
+
+        :returns: an HTTP response for request if `resp=True`
+        :rtype: `urllib3.response.HTTPResponse`
+
+        :raises: Exception if either `identity` or `key` not set
+
+        """
         self.check_id_key()
         timestamp = None
         if not props:
@@ -41,6 +81,20 @@ class ClientCompat(object):
             return response
 
     def set(self, data, path=KISSmetrics.SET_PATH, resp=False):
+        """Set a properties provided in `data` for identity.
+
+        :param data: key-value pairs to associate with identity
+        :type data: dict
+        :param path: endpoint path; defaults to ``KISSmetrics.SET_PATH``
+        :param resp: indicate whether to return response
+        :type resp: boolean
+
+        :returns: an HTTP response for request if `resp=True`
+        :rtype: `urllib3.response.HTTPResponse`
+
+        :raises: Exception if either `identity` or `key` not set
+
+        """
         self.check_id_key()
         timestamp = None
         response = self.client.set(person=self.identity, properties=data,
@@ -49,15 +103,45 @@ class ClientCompat(object):
             return response
 
     def alias(self, name, alias_to, path=KISSmetrics.ALIAS_PATH, resp=False):
+        """Map `name` to `alias_to`; actions done by one resolve to other.
+
+        :param name: consider as same individual as ``alias_to``
+        :param alias_to: consider an alias of ``name``
+        :param path: endpoint path; defaults to ``KISSmetrics.ALIAS_PATH``
+        :param resp: indicate whether to return response
+        :type resp: boolean
+
+        :returns: an HTTP response for request if `resp=True`
+        :rtype: `urllib3.response.HTTPResponse`
+
+        :raises: Exception if either `identity` or `key` not set
+
+        """
         self.check_init()
         response = self.client.alias(person=name, identity=alias_to, path=path)
         if resp:
             return response
 
     def log_file(self):
+        """Retrieve path to log file.
+
+        .. note::
+
+            Will log to ``'/tmp/kissmetrics_error.log'``; cannot be modified.
+
+        """
         return '/tmp/kissmetrics_error.log'
 
     def reset(self):
+        """Reset `identity` and `key` attributes.
+
+        .. warning::
+
+            After calling this method, further calls to `record`, `set`, &
+            `alias` will raise an `Exception`. You will need to set the API
+            key again via `key` attribute and call `identify`.
+
+        """
         self.identity = None
         self.key = None
 
