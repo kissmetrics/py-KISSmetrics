9b9bc5515b8457a39480b6341a398b111ee5037d
Ernest W. Durbin III <ewdurbin@gmail.com>
2013-11-21 08:27:58 -0500

    correct the path to README.md

--- KISSmetrics/tests/test_docs.py
+++ KISSmetrics/tests/test_docs.py
@@ -9,7 +9,7 @@ import unittest
 class DocTestCase(unittest.TestCase):
     def test_docs(self):
         failure_count, test_count \
-            = doctest.testfile('../README.md', optionflags=doctest.ELLIPSIS)
+            = doctest.testfile('../../README.md', optionflags=doctest.ELLIPSIS)
         assert failure_count == 0
 
 
