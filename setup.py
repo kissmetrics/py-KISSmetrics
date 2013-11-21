20e4ce51a68b4219efd3d463f70739b30e8076fd
Ernest W. Durbin III <ewdurbin@gmail.com>
2013-11-21 08:08:07 -0500

    pep8 and formatting for setup.py

--- setup.py
+++ setup.py
@@ -13,35 +13,39 @@ VERSION = re.compile(r".*__version__ = '(.*?)'",
                      re.S).match(fp.read()).group(1)
 fp.close()
 
-install_reqs = parse_requirements(os.path.join(base_path, 'requirements.txt'))
-test_reqs = parse_requirements(os.path.join(base_path, 'test-requirements.txt'))
+REQS_PATH = os.path.join(base_path, 'requirements.txt')
+TEST_REQS_PATH = os.path.join(base_path, 'test-requirements.txt')
+
+install_reqs = parse_requirements(REQS_PATH)
+test_reqs = parse_requirements(TEST_REQS_PATH)
 
 requirements = [str(ir.req) for ir in install_reqs]
 test_requirements = [str(ir.req) for ir in test_reqs]
 
 version = VERSION
 
-setup(name='KISSmetrics',
-      version=version,
-      description="Official KISSmetrics client library.",
-      long_description="",
-      classifiers=[
-          'Environment :: Web Environment',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: MIT License',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 3',
-          'Topic :: Internet :: WWW/HTTP',
-          'Topic :: Software Development :: Libraries',
-      ],
-      keywords='',
-      author='Ernest W. Durbin III',
-      author_email='ewdurbin@gmail.com',
-      url='https://github.com/kissmetrics/py-KISSmetrics/',
-      license='MIT',
-      packages=['KISSmetrics'],
-      install_requires=requirements,
-      tests_require=test_requirements,
-      )
+setup(
+    name='py-KISSmetrics',
+    version=version,
+    license='MIT',
+    description="Official KISSmetrics client library.",
+    long_description="",
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries',
+    ],
+    keywords='',
+    author='Ernest W. Durbin III',
+    author_email='ewdurbin@gmail.com',
+    url='https://github.com/kissmetrics/py-KISSmetrics/',
+    packages=['KISSmetrics'],
+    install_requires=requirements,
+    tests_require=test_requirements,
+)
