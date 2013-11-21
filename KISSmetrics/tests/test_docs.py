3602390d9348642ffc0c2a5b14bd1f3d4989f24b
Andrew J Lenards <andrew.lenards@gmail.com>
2013-11-21 11:26:32 -0700

    Added DESCRIPTION.rst to doctest file.

--- KISSmetrics/tests/test_docs.py
+++ KISSmetrics/tests/test_docs.py
@@ -7,9 +7,14 @@ import unittest
 # We avoid using doctest.DocFileSuite so the tests are runnable by both py.test
 # and unittest.
 class DocTestCase(unittest.TestCase):
-    def test_docs(self):
+    def test_readme(self):
         failure_count, test_count \
             = doctest.testfile('../../README.md', optionflags=doctest.ELLIPSIS)
+        assert failure_count == 0
+
+    def test_long_description(self):
+        failure_count, test_count \
+            = doctest.testfile('../../DESCRIPTION.rst', optionflags=doctest.ELLIPSIS)
         assert failure_count == 0
 
 
